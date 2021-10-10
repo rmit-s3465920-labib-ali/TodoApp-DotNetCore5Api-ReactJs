34d1c85309f0d890e5a7b594eefa7bc58a9568a1
rmit-s3465920-labib-ali <syedlabib19@gmail.com>
2021-10-10 20:52:41 +1100

    added controller, models and dbcontext.

---
+++ TodoApp-Api/Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDo_RestApi.Models;
+
+namespace ToDo_RestApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public virtual DbSet<TodoItem> Items { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
